d80f26d47e9320adbb23067ce48c0a7db94a6b7d
kaija <kaija.chang@gmail.com>
2015-07-26 23:22:23 +0800

    add basic / digest authentication

--- app.js
+++ app.js
@@ -4,9 +4,29 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var DigestStrategy = require('passport-http').DigestStrategy;
+var BasicStrategy = require('passport-http').BasicStrategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var pred_users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' },
+    { id: 2, username: 'kevin', password: 'demo', email: 'kevin@example.com' },
+  , { id: 3, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = pred_users.length; i < len; i++) {
+    var user = pred_users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
 
 var app = express();
 
@@ -20,7 +40,52 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'penroses',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+passport.use(new DigestStrategy({ qop: 'auth' },
+  function(username, done) {
+    // Find the user by username.  If there is no user with the given username
+    // set the user to `false` to indicate failure.  Otherwise, return the
+    // user and user's password.
+    findByUsername(username, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user, user.password);
+    })
+  },
+  function(params, done) {
+    // asynchronous validation, for effect...
+    process.nextTick(function () {
+      // check nonces in params here, if desired
+      return done(null, true);
+    });
+  }
+));
+passport.use(new BasicStrategy(function(username, password, done) {
+  // Find the user by username.  If there is no user with the given
+  // username, or the password is not correct, set the user to `false` to
+  // indicate failure.  Otherwise, return the authenticated `user`.
+  findByUsername(username, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+    if (user.password != password) { return done(null, false); }
+    return done(null, user);
+  })
+}));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var httpreq = require('httpreq');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//router.get('/', passport.authenticate(['basic', 'digest']),function(req, res, next) {
+router.get('/', passport.authenticate('basic'),function(req, res, next) {
+  res.send('OK');
 });
 
 module.exports = router;
